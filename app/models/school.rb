ca36fb6bed3522b46df984b54d9c9c5e9eab8c4f
Simran Kaur <simran143663@gmail.com>
2023-02-27 20:25:44 -0600

    Added controllers for all the models along with the home controller

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,5 +1,6 @@
 class School < ApplicationRecord
-  has_many:students
-  has_many:teachers
+  has_many :students
+  has_many :teachers
+  has_many :student_teachers
   validates :name, presence: true
 end
