cd9a3f8fdbeba68d8f919e43f8a04533e2a42024
Simran Kaur <simran143663@gmail.com>
2023-03-01 15:55:39 -0600

    Search bar designed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  get 'about_us/index'
+  get "about_us/index"
   root "home#index"
-  resources :schools
+
   resources :students
   resources :teachers
+
+  resources :schools, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
